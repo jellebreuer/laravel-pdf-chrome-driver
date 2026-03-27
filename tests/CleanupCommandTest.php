43f6383c10f42a3a7e4fe9d7daf6750355133e0b
Jelle Breuer <jellebreuer@gmail.com>
2026-03-27 12:43:06 +0100

    skip cleanup tests on windows

--- tests/CleanupCommandTest.php
+++ tests/CleanupCommandTest.php
@@ -9,7 +9,7 @@ it('outputs no orphaned processes when there are none', function () {
     artisan('make-pdf:cleanup')
         ->expectsOutput('No orphaned processes found.')
         ->assertSuccessful();
-});
+})->skipOnWindows();
 
 it('kills orphaned chromedriver processes', function () {
     $process = new Process([Client::chromeDriverBinary(), '--port=9599']);
@@ -25,4 +25,4 @@ it('kills orphaned chromedriver processes', function () {
     usleep(100000);
 
     expect($process->isRunning())->toBeFalse();
-});
+})->skipOnWindows();
